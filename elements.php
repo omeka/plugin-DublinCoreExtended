81fc5f9e6b14c881500facd792c0e61930e6c72e
Darío Hereñú <magallania@gmail.com>
2014-10-01 19:21:02 -0300

    Update copyright years & array 'date modified'?

--- elements.php
+++ elements.php
@@ -2,7 +2,7 @@
 /**
  * Dublin Core Extended
  * 
- * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
+ * @copyright Copyright 2007-2014 Roy Rosenzweig Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
 
@@ -92,8 +92,8 @@ $elements = array(
         '_refines'    => 'Date'
     ), 
     array(
-        'label'        => 'Date Modified', 
-        'name'        => '', 
+        'label'       => 'Date Modified', 
+        'name'        => 'modified', 
         'description' => 'Date on which the resource was changed.', 
         '_refines'    => 'Date'
     ), 
